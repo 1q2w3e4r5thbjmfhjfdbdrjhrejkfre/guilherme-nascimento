5eea6c3c67bbb192eda7a99b47c86a56f0ea20e4
1q2w3e4r5thbjmfhjfdbdrjhrejkfre <guilhermessl0345@gmail.com>
2025-02-19 15:50:52 -0300

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,18 @@
+ <H1>maria</H1>
+
+ <?php
+    //fracamente tipada
+    //TIPOS
+    $num = 1.5 ;
+    $nome = "bibia" ;
+    $status = false ;
+    $nomeCompleto = "bibizao" ;
+    //case sensiive
+    $idade = 15;
+    $Idade = 15;
+    $IDADE = 15;
+
+    echo $nome;
+ ?>
+
+ <H3></H3>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <input type="text" name =  "temperatura">
+    <select name="conversão" id="">
+     <option value="opcao">escolha uma opção</option>
+      <option value="fc">f -> c</option>
+      <option value="cf">c -> f</option>
+      <option value="ck">c -> k</option>
+
+
+
+
+
+    </select>
+</body>
+</html>
